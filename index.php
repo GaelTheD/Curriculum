ac52f21d5532f30f6159ebb1194f013f1c22a153
GaelTheD <gael_delgado2@hotmail.com>
2020-02-18 17:54:29 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Currículum</title>
+	<link rel="stylesheet" type="text/css" href="css/master.css">
+</head>
+<body bgcolor="lightyellow" class="container-portada">
+	<div class="animada">
+		<h1>
+			&nbsp;Currículum:
+		</h1>
+		<h2>
+			&nbsp;Delgado Mireles Gael
+		</h2>
+	</div>
+	<br>
+	<center>
+		<div class="principal">
+			<div class="foto" border="1">
+				<img src="img/PerroGH.png"  alt="PerroGH">
+			</div>
+			<div class="info-container">
+				Información personal:
+				<blockquote>	
+					<p>
+						Nombre: Gael Delgado Mireles
+					</p>
+					<p>
+						Edad: 17 años
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						Sexo: Hombre
+					</p>
+				</blockquote>
+				Objetivo profesional:
+				<blockquote>
+					<p>
+						Convertirme en un gran desarrollador de software, que pueda estar a la vanguardia de las nuevas
+						tecnologías.
+					</p>	
+				</blockquote>
+				Habilidades y conocimientos:
+				<blockquote>
+					Sé manejar HTML, CSS 3, soy bueno en inglés, me sé todos los trucos del GTA SA.
+					<p>
+						Lenguajes de programación que manejo:
+					</p>
+					<p>
+						<ol>
+							<li>
+								Java
+							</li>
+							<li>
+								Python
+							</li>
+							<li>
+								Javascript
+							</li>
+						</ol>
+					</p>
+				</blockquote>
+			</div>
+		</div>
+	</center>
+	<div class="loader-doble"></div>
+	<footer class="footer">
+		<br>
+		<blockquote>
+			Contacto:
+			<p>
+				Correo Electrónico: gael_delgado2@hotmail.com
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				Teléfono: 55 3590 4764
+			</p>
+		</blockquote>
+	</footer>
+</body>
+</html>
